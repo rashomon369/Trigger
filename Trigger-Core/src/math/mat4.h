34d52f9d179a4677974290625d6a96cc3f3ab27a
rashomon369 <wang39@gmail.com>
2016-09-23 22:08:19 -0700

    9/23 Implemented further matrix functionalities include multiply, rotation, translation, scale, etc.

--- Trigger-Core/src/math/mat4.h
+++ Trigger-Core/src/math/mat4.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#define _USE_MATH_DEFINES
+#include <math.h>
 #include "maths.h"
 
 namespace trigger {
--- Trigger-Core/src/math/maths.h
+++ Trigger-Core/src/math/maths.h
@@ -3,3 +3,9 @@
 #include "vector3.h"
 #include "vector4.h"
 #include "mat4.h"
+
+
+float toRadians(float degrees)
+{
+	return degrees * M_PI / 180.0f;
+}
